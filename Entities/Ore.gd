2a29d4a233784256b0b701a7e6aa9bbc61a2acb3
odedro987 <odedro987@gmail.com>
2018-09-07 23:17:28 +0300

    SpaceMiner rotates to mouse.

--- Entities/Ore.gd
+++ Entities/Ore.gd
@@ -1,11 +1,19 @@
 extends Node2D
 
 enum OreType {
-	COPPER,
-	IRON,
-	GOLD,
-	DIAMOND
+	COPPER = 5,
+	IRON = 10,
+	GOLD = 40,
+	DIAMOND = 100,
+	SPACE_ROCK = 0
 }
 
+export(OreType) var type
+
 func _ready():
-	pass
+	if(type == null): type = OreType.SPACE_ROCK
+	print(type)
+	drawOre()
+
+func drawOre():
+	get_node("Sprite").modulate = Color(type / 255.0, type / 255.0 * 10, type / 255.0, 1.0)
--- Entities/SpaceMiner.gd
+++ Entities/SpaceMiner.gd
@@ -1,15 +1,15 @@
 extends KinematicBody2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var velocity = Vector2(0, 0)
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	set_physics_process(true)
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _physics_process(delta):
+	#get_node("Sprite").rotation = 0 if get_node("Sprite").rotation >= 360 else get_node("Sprite").rotation + delta
+	move_and_collide(velocity * speed * delta)
+	seek(get_global_mouse_position())
+
+func seek(target):
+	global_rotation_degrees = rad2deg(global_position.angle_to_point(target)) - 90
+	velocity = (target - global_position)
